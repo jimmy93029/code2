7abd41531bcd15299dc9af6613f0b160e33b1ddb
jimmy <jimmywu0229@gmail.com>
2022-06-23 12:06:38 +0800

    add new file

---
+++ code2.py
@@ -0,0 +1,14 @@
+attempt = 1
+while True:
+  if attempt == 4:
+      break
+  else:
+    reply = input('請輸入7位密碼')
+    if reply == 'a123456':
+      print('登入成功')
+      break
+    else:
+      print('密碼錯誤 ，還有',3-attempt,'次機會')
+      attempt = attempt + 1
+    
+    
